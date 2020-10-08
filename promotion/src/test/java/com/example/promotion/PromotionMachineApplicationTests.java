3e50536df8aff2c5632eb9363fbe18604e176d23
bhasan02 <sangik.bhattacharjee@csgi.com>
2020-10-08 16:04:22 +0530

    Implementing promotions TDD1

--- promotion/src/test/java/com/example/promotion/PromotionMachineApplicationTests.java
+++ promotion/src/test/java/com/example/promotion/PromotionMachineApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.promotion;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.promotion.controller.PromotionController;
+import com.example.promotion.helper.ResponseDetail;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
 class PromotionMachineApplicationTests {
+	
+	@Autowired
+	private PromotionController promotionController;
 
 	@Test
 	void contextLoads() {
+		ResponseDetail response=promotionController.fetchFinalPrice(1, 1, 1, 0);
+		Assert.assertEquals(response.getTotalAmount(), 100);
+		
 	}
+	
+	
 
 }
